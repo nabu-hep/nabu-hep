6e63c059fac21d765f518bd44f65c5bde803078d
Jack Y. Araz <jackaraz@gmail.com>
2024-11-22 16:35:16 -0500

    update

--- nabu/transform_base.py
+++ nabu/transform_base.py
@@ -1,12 +1,55 @@
-from typing import Any
+from typing import Any, Literal
+
 import jax.numpy as jnp
 import numpy as np
+
+from .dalitz_utils import dalitz_to_square, square_to_dalitz
 
 Array = Any
 
 
-class PosteriorTransformBase:
+class PosteriorTransform:
     """Base class to handle transformations"""
+
+    def __init__(self, name: Literal["mean_std", "dalitz"] = None, **kwargs):
+        if name == "mean_std":
+            mean, scale = jnp.array(kwargs["mean"]), jnp.array(kwargs["std"])
+
+            def forward(x):
+                return (x - mean) / scale
+
+            def backward(x):
+                return x * scale + mean
+
+        elif name == "dalitz":
+            md, ma, mb, mc = (
+                kwargs["md"],
+                kwargs["ma"],
+                kwargs["mb"],
+                kwargs["mc"],
+            )
+
+            def forward(x):
+                return dalitz_to_square(x, md, ma, mb, mc)
+
+            def backward(x):
+                return square_to_dalitz(x, md, ma, mb, mc)
+
+        elif name is None:
+            forward = lambda x: x
+            backward = lambda x: x
+
+        else:
+            raise NotImplementedError(f"{name} currently not implemented")
+
+        self._forward = forward
+        self._backward = backward
+        self._meta = {"name": name}
+        self._meta.update({"kwargs": kwargs})
+
+    def serialise(self) -> dict:
+        """Serialise transformer"""
+        return self._meta
 
     def forward(self, x: Array) -> Array:
         """
@@ -20,7 +63,7 @@ class PosteriorTransformBase:
             ``Array``:
             Transformed data
         """
-        return x
+        return jnp.array(self.forward(x))
 
     def backward(self, y: Array) -> Array:
         """
@@ -34,30 +77,4 @@ class PosteriorTransformBase:
             ``Array``:
             Original data
         """
-        return y
-
-    def _forward(self, x: Array) -> jnp.ndarray:
-        """
-        Preloader for forward transformer.
-
-        Args:
-            x (``Array``): input data
-
-        Returns:
-            ``jnp.ndarray``:
-            Transformed data
-        """
-        return jnp.array(self.forward(x))
-
-    def _backward(self, y: jnp.array) -> np.ndarray:
-        """
-        Preloader for backward transformer.
-
-        Args:
-            y (``Array``): transformed data
-
-        Returns:
-            ``np.ndarray``:
-            _description_
-        """
-        return np.array(self.backward(np.array(y)))
+        return jnp.array(self._backward(y))
