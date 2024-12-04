fdbd16a49e126d3db064656912cc59e6350049f0
Jack Y. Araz <jackaraz@gmail.com>
2024-12-04 17:51:46 -0500

    update testing

--- test/test_fit_script.py
+++ test/test_fit_script.py
@@ -8,7 +8,9 @@ from nabu.goodness_of_fit import Histogram
 def test_fit_script_output():
     """Test fit script output"""
     try:
-        deviations = np.load("./test/results/TEST-RESULT/deviations.npz")["deviations"]
+        deviations = np.load("./test/results/TEST-RESULT-COMPLETE/deviations.npz")[
+            "deviations"
+        ]
     except FileNotFoundError:
         assert False, "Please execute pytest from the main folder"
 
@@ -20,8 +22,8 @@ def test_fit_script_output():
     hist = Histogram(dim=deviations.shape[1], bins=bins, vals=chi2_test)
 
     assert np.isclose(
-        hist.residuals_pvalue, 0.019739547852598904
+        hist.residuals_pvalue, 4.0564689434474666e-05
     ), f"p-val for residuals are wrong, {hist.residuals_pvalue}"
     assert np.isclose(
-        hist.kstest_pval, 0.006416630663697942
+        hist.kstest_pval, 0.19517684699261595
     ), f"p-val for KST is wrong, {hist.kstest_pval}"
