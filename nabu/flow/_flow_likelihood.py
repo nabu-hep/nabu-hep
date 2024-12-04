d1c075a28a4f30acc0a8e0f54407c7c1206d03a7
Jack Y. Araz <jackaraz@gmail.com>
2024-12-04 17:08:32 -0500

    update likelihoods

--- nabu/flow/_flow_likelihood.py
+++ nabu/flow/_flow_likelihood.py
@@ -1,14 +1,22 @@
+import jax.random as jr
+import numpy as np
+import optax
 from flowjax.distributions import Transformed
 from jax import vmap
 
 from nabu import Likelihood
 from nabu.transform_base import PosteriorTransform
 
+from ._train import fit, get_optimizer
+
 __all__ = ["FlowLikelihood"]
 
 
 def __dir__():
     return __all__
+
+
+# pylint: disable=arguments-differ,too-many-arguments
 
 
 class FlowLikelihood(Likelihood):
@@ -50,3 +58,59 @@ class FlowLikelihood(Likelihood):
                 txt += f"{item}"
             txt += ",\n"
         return txt + ")"
+
+    def fit_to_data(
+        self,
+        dataset: np.ndarray,
+        condition: np.ndarray = None,
+        learning_rate: float = 1e-4,
+        optimizer: str = "adam",
+        lr_scheduler: optax.Schedule = None,
+        max_epochs: int = 100,
+        max_patience: int = 5,
+        check_every: int = 1,
+        batch_size: int = 100,
+        validation_probability: float = 0.1,
+        verbose: bool = True,
+        random_seed: int = np.random.randint(0, high=999999999999),
+    ) -> dict[str, list[float]]:
+        """
+        Fit likelihood to the data
+
+        Args:
+            dataset (``np.ndarray``): _description_
+            condition (``np.ndarray``, default ``None``): _description_
+            learning_rate (``float``, default ``1e-4``): _description_
+            optimizer (``str``, default ``"adam"``): _description_
+            ls_scheduler (``optax.Schedule``, default ``None``): _description_
+            max_epochs (``int``, default ``100``): _description_
+            max_patience (``int``, default ``5``): _description_
+            check_every (``int``, default ``1``): _description_
+            batch_size (``int``, default ``100``): _description_
+            validation_probability (``float``, default ``0.1``): _description_
+            verbose (``bool``, default ``True``): _description_
+            random_seed (``int``): _description_
+
+        Returns:
+            ``dict[str, list[float]]``:
+            Training history
+        """
+        optimizer = optax.inject_hyperparams(get_optimizer(optimizer))(
+            learning_rate=learning_rate
+        )
+        flow, history = fit(
+            key=jr.key(random_seed),
+            dist=self.model,
+            x=self.transform.backward(dataset),
+            condition=condition,
+            optimizer=optimizer,
+            max_epochs=max_epochs,
+            max_patience=max_patience,
+            batch_size=batch_size,
+            val_prop=validation_probability,
+            lr_scheduler=lr_scheduler,
+            show_progress=verbose,
+            check_every=check_every,
+        )
+        self.model = flow
+        return history
