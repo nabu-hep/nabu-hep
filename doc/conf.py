9cee36d7c80430905e23022a4e505b36b5e39bc3
Jack Y. Araz <jackaraz@gmail.com>
2024-12-13 07:38:49 -0500

    precomit update

--- doc/conf.py
+++ doc/conf.py
@@ -2,23 +2,23 @@ import nabu
 
 from nabu._version import __version__
 
-project = 'NABU'
-copyright = '2024, The NABU Authors'
-author = 'Jack Y. Araz and others'
-master_doc = 'index'
+project = "NABU"
+copyright = "2024, The NABU Authors"
+author = "Jack Y. Araz and others"
+master_doc = "index"
 
 release = __version__
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.todo',
-    'sphinx.ext.autosectionlabel',
-    'sphinxarg.ext'
+    "sphinx.ext.autodoc",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.todo",
+    "sphinx.ext.autosectionlabel",
+    "sphinxarg.ext",
 ]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
