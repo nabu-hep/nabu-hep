b5910c9a32f5de5561b4d73e57d4b1595c219f2d
Jack Y. Araz <jackaraz@gmail.com>
2025-01-27 15:59:54 -0500

    update doc and reporting

--- nabu/tensorboard.py
+++ nabu/tensorboard.py
@@ -4,31 +4,58 @@ from tensorboard.compat import tf2 as tf
 
 
 class SummaryWriter:
+    """Summary writer for tensorboard"""
+
     def __init__(self, log: str):
         if log is None:
             self.writer = None
         else:
             self.writer = {
-                "gen": tf.summary.create_file_writer(log),
+                "summary": tf.summary.create_file_writer(log),
                 "train": tf.summary.create_file_writer(os.path.join(log, "train")),
                 "val": tf.summary.create_file_writer(os.path.join(log, "val")),
             }
 
-    def add_history(self, history: dict[str, list[float]], step: int):
+    def add_history(self, history: dict[str, list[float]], step: int) -> None:
+        """
+        Add history to tensorboard
+
+        Args:
+            history (``dict[str, list[float]]``): history of the training
+            step (``int``): current step
+        """
         if self.writer is not None:
             with self.writer["train"].as_default():
                 tf.summary.scalar("loss", history["train"][-1], step=step)
             with self.writer["val"].as_default():
                 tf.summary.scalar("loss", history["val"][-1], step=step)
-            with self.writer["gen"].as_default():
+            with self.writer["summary"].as_default():
                 tf.summary.scalar("learning_rate", history["lr"][-1], step=step)
 
-    def scalar(self, name: str, value: float, step: int, tag: str = "gen"):
+    def scalar(self, name: str, value: float, step: int, tag: str = "summary") -> None:
+        """
+        Add scalar to tensorboard
+
+        Args:
+            name (``str``): name of the scalar
+            value (``float``): value of the scalar
+            step (``int``): step
+            tag (``str``, default ``"summary"``):  tag
+        """
         if self.writer is not None:
             with self.writer[tag].as_default():
                 tf.summary.scalar(name, value, step=step)
 
-    def text(self, name, data, step=None, tag: str = "gen"):
+    def text(self, name: str, data: str, step: int = None, tag: str = "summary") -> None:
+        """
+        Add text to tensorboard
+
+        Args:
+            name (``str``): name of the text
+            data (``str``): data
+            step (``int``, default ``None``): step
+            tag (``str``, default ``"summary"``): tag
+        """
         if self.writer is not None:
             with self.writer[tag].as_default():
                 tf.summary.text(name, data, step=step)
@@ -40,8 +67,19 @@ class SummaryWriter:
         step: int,
         bins: int = 30,
         description: str = None,
-        tag: str = "gen",
-    ):
+        tag: str = "summary",
+    ) -> None:
+        """
+        Add histogram to tensorboard
+
+        Args:
+            name (``str``): name of the histogram
+            data (``list[float]``): data
+            step (``int``): step
+            bins (``int``, default ``30``): number of bins
+            description (``str``, default ``None``): description
+            tag (``str``, default ``"summary"``): tag
+        """
         if self.writer is not None:
             with self.writer[tag].as_default():
                 tf.summary.histogram(
